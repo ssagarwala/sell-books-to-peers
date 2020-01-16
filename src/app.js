a928e918ca2956763a87d77d8c34b42328d9f2fb
Sehar <sehar.sagarwala@gmail.com>
2020-01-15 21:11:42 -0800

    outline of main page components

--- src/app.js
+++ src/app.js
@@ -1,32 +1,34 @@
-const app = {
-    title: 'Sell Books To Peers',
-    subtitle : 'Sell Books To Peers'
+class Header extends React.Component{
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
 }
-const user = {
-    name: 'abc',
-    password : 'password',
-    books: [
-           'book1',
-           'book2',
-           'book3'
-    ]
+class Books extends React.Component{
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+class AddBook extends React.Component{
+    rendre(){
+        return(
+            <div>
+            </div>
+        )
+    }
 }
 
-const getBooks = (user)=>{
-    return  user.books.map((book)=>{
-            return <li key={book}>{book}</li>;
-        });
-}
- 
-const appJSX = (
-        <div>
-            <h1>{app.title}</h1>
-            <h2>{app.subtitle}</h2>
-            {user.password === 'password' && <strong> Welcome {user.name} </strong>}
-            {user.books.length>0 ? 'Here are your books': 'You have no books'}
-            {getBooks(user)}
-        </div>
-)
-const appRoot = document.getElementById('app-root');
+const jsx = (
+    <div>
+        <Header />
+        <Books />
+        <AddBook />
+    </div>
+);
 
-ReactDOM.render(appJSX,appRoot);
+ReactDOM.render(jsx, document.getElementById('app'));
